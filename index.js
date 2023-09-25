df541e14c04c239d414cb9f10026d70d05b0c795
ZidniGz <143054285+ZidniGz@users.noreply.github.com>
2023-09-25 17:16:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 80;
 let result = require("./db/file.json");
 
 // Create folder
-//if (!fs.existsSync('./public/file')) fs.mkdirSync('./public/file')
+if (!fs.existsSync(path.join(process.cwd(), "public/file")) fs.mkdirSync(path.join(process.cwd(), "public/file"))
 
 function makeid(length) {
     let result = '';
