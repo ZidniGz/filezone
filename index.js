10402dd78e8d9d236e40b61043c5e295edb983e3
ZidniGz <143054285+ZidniGz@users.noreply.github.com>
2023-09-26 09:29:38 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const app = require ("express")()
+const fs = require("fs")
 
 app.get('/', (req,res) => res.send('GET'))
 app.post('/',(req,res) => res.send('POST'))
+app.get('/data', (req, res) => {
+
+let data = fs.writeFileSync('./data.txt','halo dari get')
+res.send('GET')
+})
+app.post('/data', (req, res) => {
+
+let data = fs.readFileSync('./data.txt')
+res.send(data)
+})
+
 app.listen(4000)
