71e6f4c400b8d8186b3a9a44b1af4361446ea7f6
ZidniGz <143054285+ZidniGz@users.noreply.github.com>
2023-12-16 17:27:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const tiktok = async (url) => {
     }
 	
     let anu = await got(`https://api16-core-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${result}&version_name=1.0.4&version_code=104&build_number=1.0.4&manifest_version_code=104&update_version_code=104&openudid=4dsoq34x808ocz3m&uuid=6320652962800978&_rticket=1671193816600&ts=1671193816&device_brand=POCO&device_type=surya&device_platform=android&resolution=1080*2179&dpi=440&os_version=12&os_api=31&carrier_region=US&sys_region=US%C2%AEion=US&app_name=FrierenDv&app_language=en&language=en&timezone_name=Western%20Indonesia%20Time&timezone_offset=25200&channel=googleplay&ac=wifi&mcc_mnc=&is_my_cn=0&aid=1180&ssmix=a&as=a1qwert123&cp=cbfhckdckkde1`).json()
-return anu;
+return { id: result, data: anu}
 				      };
 
 const stiker = (file, stickerMetadata = {
