c6fec435fb669c6a53aee18235fa722709e1fdf3
ZidniGz <143054285+ZidniGz@users.noreply.github.com>
2023-12-16 19:02:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ if (!req.header('author')) return res.send('false')
 let data = req.files.file.data
 let pack = req.header('packname')
 let author = req.header('author')
-let rs = await stiker(data,{pack: pack ? pack : '', author: auth ? auth : '',keepScale: true, circle: false
+let rs = await stiker(data,{pack: pack ? pack : '', author: author ? author : '',keepScale: true, circle: false
                                                                                })
 res.send(rs)
 })
