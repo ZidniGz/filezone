0a15bc9e7d10a20a5f8f3954889c0f4c76bf4991
ZidniGz <143054285+ZidniGz@users.noreply.github.com>
2023-12-16 16:57:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,38 @@ app.use(fileUpload({
 const image = async(q)=>{
 	return await got.post("https://freeimagegenerator.com/queries/queryCreateAIImagesFromTextPrompt.php?server=1", {form:{action:"createAIImages", aiPrompt:q ? q : 'ayam'}}).json()
 }
+
+const tiktok = async (url) => {
+    let result;
+    const REGEXP = /video\/([\d|\+]+)?\/?/;
+    const valid = url.match(REGEXP);
+    if (valid) {
+        return valid[1];
+    }
+    else {
+        result = false;
+        try {
+            const data = await axios.default
+                .get(url, {
+                headers: {
+                    'Accept-Encoding': 'deflate',
+                },
+                maxRedirects: 0,
+            })
+                .catch((e) => { var _a, _b; return (_b = (_a = e === null || e === void 0 ? void 0 : e.response) === null || _a === void 0 ? void 0 : _a.headers) === null || _b === void 0 ? void 0 : _b.location; });
+            if (data) {
+                const _url = data;
+                const _valid = _url.match(REGEXP);
+                if (_valid) {
+                    result = _valid[1];
+                }
+            }
+        }
+        catch { }
+    }
+    return axios.get(https://api16-core-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${result}&version_name=1.0.4&version_code=104&build_number=1.0.4&manifest_version_code=104&update_version_code=104&openudid=4dsoq34x808ocz3m&uuid=6320652962800978&_rticket=1671193816600&ts=1671193816&device_brand=POCO&device_type=surya&device_platform=android&resolution=1080*2179&dpi=440&os_version=12&os_api=31&carrier_region=US&sys_region=US%C2%AEion=US&app_name=FrierenDv&app_language=en&language=en&timezone_name=Western%20Indonesia%20Time&timezone_offset=25200&channel=googleplay&ac=wifi&mcc_mnc=&is_my_cn=0&aid=1180&ssmix=a&as=a1qwert123&cp=cbfhckdckkde1).then({data} => data)
+};
+
 const stiker = (file, stickerMetadata = {
 				author: '‎',
 				pack: '‎',
@@ -89,6 +121,12 @@ let pack = req.header('packname')
 let author = req.header('author')
 let rs = await stiker(data,{pack:pack, author: author, keepScale:true,circle:false})
 res.send(rs)
+})
+appp.get("/tiktok", async (req,res)=> {
+	if (!req.query.url) return res.json({msg:false});
+	let data = await tiktok(req.query.url)
+	if (!data) return res.json({msg:false})
+	res.json(data)
 })
 app.get("/text-image", async(req,res)=> {
 let q = req.query.q
