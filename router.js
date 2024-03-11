f7ac681da9820c309f928b5e107592072adbacf5
ZidniGz <143054285+ZidniGz@users.noreply.github.com>
2024-03-11 19:55:35 +0700

    Add files via upload

---
+++ router.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+const monk = require('monk');
+
+const db = monk('mongodb+srv://caliph71:clph1122@cluster0.e1ccz.mongodb.net/myFirstDatabase'); // Ganti dengan URL MongoDB Anda
+
+
+const collection = db.get('crud-api'); 
+
+// Middleware untuk router ini
+router.use((req, res, next) => {
+    console.log('Middleware for dataRouter');
+    next();
+});
+
+// Routes untuk router ini
+router.get('/', async(req, res) => {
+    const data = await collection.find();
+    res.json(data);
+});
+
+
+router.post('/', async (req, res) => {
+    const newData = req.body;
+    await collection.insert(newData);
+    res.status(201).send();
+});
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const updatedData = req.body;
+    await collection.update({ _id: id }, { $set: updatedData });
+    res.send();
+});
+
+// Delete data
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    await collection.remove({ _id: id });
+    res.send();
+});
+
+
+module.exports = router;
