65a927b96fd52eebb62d302df6882a62843dc1a3
ZidniGz <143054285+ZidniGz@users.noreply.github.com>
2024-03-27 18:16:31 +0700

    Update index.js
    oo

--- index.js
+++ index.js
@@ -230,8 +230,8 @@ app.get("/convert", async (req,res)=>{
   console.log(id);console.log(q);
   if (!id) return res.json({msg:false});
   let data = await convert2(id,q)
-  let buffer =await got(data.dlink, {referer: "https://y2mate.com"})
-  res.send(data.body)
+  let buffer =await got(data.dlink, {referer: "https://y2mate.com"}).buffer()
+  res.send(data)
 })
 
 app.get("/tiktok", async (req, res) => {
