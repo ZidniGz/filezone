e8d78d183bc49e6d375c2ce47dc9ce0aa54fdbb3
ZidniGz <143054285+ZidniGz@users.noreply.github.com>
2024-03-29 14:42:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({limit: '100mb'}));
 async function getTranscript(e){try{const t=await ytdl.getInfo(e);if(!("captions"in t.player_response))return;const a=t.player_response.captions.playerCaptionsTracklistRenderer.captionTracks[0].baseUrl,r=await fetch(a),n=await r.text();return new Promise(((e,t)=>{xml2js.parseString(n,((a,r)=>{if(a)t(a);else{const t=r.transcript.text.map((e=>e._)).join(" ").replace(/&#39;/g,"'").replace(/i /g,"I ").replace(/ \[Music\]/g,"");e(t)}}))}))}catch(e){}}
 const processImg=async(e,n)=>new Promise((async(t,o)=>{let r=["enhance","recolor","dehaze"];n=r.includes(n)?n:r[0];let a=new require("form-data")(),i="https://inferenceengine.vyro.ai/"+n;a.append("model_version",1,{"Content-Transfer-Encoding":"binary",contentType:"multipart/form-data; charset=uttf-8"}),a.append("image",Buffer.from(e),{filename:"enhance_image_body.jpg",contentType:"image/jpeg"}),a.submit({url:i,host:"inferenceengine.vyro.ai",path:"/"+n,protocol:"https:",headers:{"User-Agent":"okhttp/4.9.3",Connection:"Keep-Alive","Accept-Encoding":"gzip"}},(function(e,n){e&&o();let r=[];n.on("data",(function(e,n){r.push(e)})).on("end",(()=>{t(Buffer.concat(r))})),n.on("error",(e=>{o()}))}))}));
-cosnt igdl = function (url){
+const igdl = function (url){
 return new Promise(async(reject, resolve) => {
 try {
   const jsonObject = {
